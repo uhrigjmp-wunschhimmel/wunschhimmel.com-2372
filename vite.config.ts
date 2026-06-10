f07f3dc32ce3aa8f1e15cc5b0a2341ac474fba52
uhrigjmp-wunschhimmel <uhrigjmp@aol.com>
2026-06-10 14:40:33 +0200

    fix: Runable Analytics entfernt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwind from "@tailwindcss/vite"
 import path from "path";
-import runableAnalyticsPlugin from "./vite/plugins/runable-analytics-plugin";
+
 
 export default defineConfig({
-	plugins: [react(), runableAnalyticsPlugin(), cloudflare(), tailwind()],
+	plugins: [react(), cloudflare(), tailwind()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src/web"),
