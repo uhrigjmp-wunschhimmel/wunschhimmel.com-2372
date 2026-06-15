cb373fe3b2953e1d51400cb4f5f84ff36cd9c53b
uhrigjmp-wunschhimmel <uhrigjmp@aol.com>
2026-06-15 15:24:24 +0200

    feat: Footer global in App.tsx eingebaut

--- src/web/app.tsx
+++ src/web/app.tsx
@@ -4,6 +4,7 @@ import { AgentFeedback } from "@runablehq/website-runtime";
 import { I18nProvider } from "./lib/i18n";
 import { ThemeProvider } from "./lib/theme";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 import { CookieBanner } from "./components/CookieBanner";
 import { WunschengelChat } from "./components/WunschengelChat";
 import { Toaster } from "./components/ui/sonner";
@@ -28,38 +29,40 @@ import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-   <ErrorBoundary>
-     <Provider>
-      <I18nProvider>
-        <ThemeProvider>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={LandingPage} />
-          <Route path="/sign-in" component={SignIn} />
-          <Route path="/sign-up" component={SignUp} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/list/:id" component={ListDetail} />
-          <Route path="/shared/:token" component={SharedList} />
-          <Route path="/explore" component={Explore} />
-          <Route path="/impressum" component={Impressum} />
-          <Route path="/datenschutz" component={Datenschutz} />
-	  <Route path="/agb" component={AGBPage} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/feed" component={Feed} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-          <Route path="/reset-password" component={ResetPassword} />
-          <Route path="/share-target" component={ShareTarget} />
-          <Route component={NotFoundPage} />
-        </Switch>
-        <CookieBanner />
-        <WunschengelChat />
-        <Toaster richColors position="bottom-center" />
-        {import.meta.env.DEV && <AgentFeedback />}
-        </ThemeProvider>
-      </I18nProvider>
-    </Provider>
-   </ErrorBoundary>
+    <ErrorBoundary>
+      <Provider>
+        <I18nProvider>
+          <ThemeProvider>
+            <Navbar />
+            <Switch>
+              <Route path="/" component={LandingPage} />
+              <Route path="/sign-in" component={SignIn} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/list/:id" component={ListDetail} />
+              <Route path="/shared/:token" component={SharedList} />
+              <Route path="/explore" component={Explore} />
+              <Route path="/impressum" component={Impressum} />
+              <Route path="/datenschutz" component={Datenschutz} />
+              <Route path="/agb" component={AGBPage} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/feed" component={Feed} />
+              <Route path="/admin" component={Admin} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+              <Route path="/reset-password" component={ResetPassword} />
+              <Route path="/share-target" component={ShareTarget} />
+              <Route component={NotFoundPage} />
+            </Switch>
+            <Footer />
+            <CookieBanner />
+            <WunschengelChat />
+            <Toaster richColors position="bottom-center" />
+            {import.meta.env.DEV && <AgentFeedback />}
+          </ThemeProvider>
+        </I18nProvider>
+      </Provider>
+    </ErrorBoundary>
   );
 }
+
 export default App;
